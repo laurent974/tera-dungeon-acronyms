00dc190f071f1af16f58ea639a82d96d6ea8b408
Laurent <lyenpon974@gmail.com>
2023-09-16 21:20:17 +0200

    Add - MCH to manaya

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function dungeonAcronym(mod) {
 			mod.command.message('<font color="#FF007F">Liste des accronymes des donjons</font>' )
 			mod.command.message('<font color="#BEA9DE">SSH</font> -> <font color="#FFDCDB">Temple Sanglant de Dkurion HM</font> ' )
 			mod.command.message('<font color="#BEA9DE">VHH</font> -> <font color="#FFDCDB">Porte souterraine effondrée de Velika</font> ' )
-			mod.command.message('<font color="#BEA9DE">Plexus</font> -> <font color="#FFDCDB">Noyau de Manaya (difficile)</font> ' )
+			mod.command.message('<font color="#BEA9DE">Plexus / MCH</font> -> <font color="#FFDCDB">Noyau de Manaya (difficile)</font> ' )
 			mod.command.message('<font color="#BEA9DE">DS</font> -> <font color="#FFDCDB">Pic de l\'effroi</font> ' )
 			mod.command.message('<font color="#BEA9DE">DRCH</font> -> <font color="#FFDCDB">Forteresse de Ténébris (difficile)</font> ' )
 		}
