8615a456eaf7ce423e1f26d7a890e9b1bbc55df1
Laurent <lyenpon974@gmail.com>
2023-09-24 14:35:58 +0200

    Update - Added new dungeons

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ module.exports = function dungeonAcronym(mod) {
   mod.command.add('dj', {
 		'$default': () => {
 			mod.command.message('<font color="#FF007F">Liste des accronymes des donjons</font>' )
+			mod.command.message('<font color="#BEA9DE">LDH</font> -> <font color="#FFDCDB">Rêve de Rukmia HM</font> ' )
+			mod.command.message('<font color="#BEA9DE">VSH</font> -> <font color="#FFDCDB">Sanctuaire effondré HM</font> ' )
 			mod.command.message('<font color="#BEA9DE">SSH</font> -> <font color="#FFDCDB">Temple Sanglant de Dkurion HM</font> ' )
-			mod.command.message('<font color="#BEA9DE">VHH</font> -> <font color="#FFDCDB">Porte souterraine effondrée de Velika</font> ' )
 			mod.command.message('<font color="#BEA9DE">Plexus / MCH</font> -> <font color="#FFDCDB">Noyau de Manaya (difficile)</font> ' )
 			mod.command.message('<font color="#BEA9DE">DS</font> -> <font color="#FFDCDB">Pic de l\'effroi</font> ' )
 			mod.command.message('<font color="#BEA9DE">DRCH</font> -> <font color="#FFDCDB">Forteresse de Ténébris (difficile)</font> ' )
+			mod.command.message('<font color="#BEA9DE">VHH</font> -> <font color="#FFDCDB">Porte souterraine effondrée de Velika</font> ' )
 		}
 	})
 }
